d90bcd4798c4512bdabddcf01b7673b5abd55b51
Joe Fioti <jafioti@gmail.com>
2025-09-14 10:06:05 -0700

    fixed codegen kernel assignment issue

--- crates/luminal_2/src/extract.rs
+++ crates/luminal_2/src/extract.rs
@@ -217,17 +217,6 @@ fn extract_trajectories<'a>(
                     )
                     .map(|i| vec![i])
                     .unwrap_or_default()
-                } else if egraph.nodes[child_first_enode].op == "Loop" {
-                    // Pull just the range out for the loop
-                    extract_shortest(
-                        egraph,
-                        egraph.nid_to_cid(&egraph.nodes[child_first_enode].children[1]),
-                        seen,
-                        junk_cache,
-                        &mut FxHashMap::default(),
-                    )
-                    .map(|i| vec![i])
-                    .unwrap_or_default()
                 } else {
                     extract_trajectories(
                         egraph,
@@ -312,6 +301,7 @@ pub fn search(
         &mut FxHashMap::default(),
         1,
     );
+    println!("traj: {}", trajectories.len());
     // build loop level -> enode mapping
     let mut loop_level_values = FxHashMap::default();
     for (id, _) in &egraph.class_data {
@@ -345,7 +335,11 @@ pub fn search(
         // make sure all IR nodes loop levels
         for (id, node) in &egraph.nodes {
             if node.eclass.to_string().starts_with("IR-") {
-                assert!(loop_level_map.contains_key(id));
+                assert!(
+                    loop_level_map.contains_key(id),
+                    "Loop level not found for {}",
+                    node.op
+                );
             }
         }
     }
@@ -374,6 +368,7 @@ pub fn search(
         // crate::egraph_debugger::display_egraph_with_path(&egraph, &trajectory);
         // Build termdag
         let Some(graph) = extraction_to_graph(&egraph, &trajectory, &loop_level_map) else {
+            println!("skip");
             continue;
         };
         prev_graphs.push(graph.clone());
@@ -853,7 +848,12 @@ pub fn extraction_to_graph(
                     r
                 }
             }
-            _ => return None, // other => panic!("unsupported IR op '{other}'"),
+            op => {
+                if option_env!("PRINT_REJECT").is_some() {
+                    println!("unsupported IR op: {op}");
+                }
+                return None;
+            }
         })
     }
 
